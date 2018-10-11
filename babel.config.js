4b7df89169a46f1d1afd48676c9f8ac2d20f34f3
Eva-Liisa Edela <evaliisae@Eva-Liisas-MacBook-Air.local>
2018-10-11 13:56:52 +0300

    Numbrite faktide lisamine

--- babel.config.js
+++ babel.config.js
@@ -3,3 +3,6 @@ module.exports = {
     '@vue/app'
   ]
 }
+//ES5 ja ES6 arrow functions on ainult ES6, 
+//mõned brauserid ei tunnista veel ES6 ja 
+//selle faili abil saame konvertida ES5'ks
---
+++ src/components/Faktid.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Numbrite faktid</h1>
+    <p>Huvitavad faktid iga numbri kohta</p>
+    <input type="number" min="0" v-model="number">
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+  const axios = require('axios') //kui tood mujalt andmed sisse
+export default {
+  name: 'Faktid',
+  data () {
+    return {
+      data: '',
+      number: 0
+  
+    }
+  },
+  watch: {
+    number() {
+      if(this.number !== '') {
+      
+      var url = `http://numbersapi.com/${this.number}`
+
+      axios.get(url)
+        .then(response => {
+          this.data = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+       } 
+    }
+  }
+}
+</script>
+<!--this.users... kasutab siis seda kasutajat, defineerib-->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
